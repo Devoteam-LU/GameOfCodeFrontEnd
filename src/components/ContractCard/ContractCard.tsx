1e6f656a4cbc5a2f18126abfffb14828ad8bcd15
Kevin Gonzalez <typekev@gmail.com>
2020-10-11 02:37:06 +0200

    Adds contract cards

---
+++ src/components/ContractCard/ContractCard.tsx
@@ -0,0 +1,79 @@
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonItem,
+} from "@ionic/react";
+import React from "react";
+import { ContractDto } from "../../api-clients/api";
+
+interface IProps {
+  contract: ContractDto;
+  isNew?: boolean;
+}
+
+export default function ContractCard({ contract, isNew }: IProps) {
+  const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(
+    contract.creationDate
+  );
+  const mo = new Intl.DateTimeFormat("en", { month: "short" }).format(
+    contract.creationDate
+  );
+  const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(
+    contract.creationDate
+  );
+  const date = `${da}.${mo}.${ye}`;
+
+  return (
+    <IonCard>
+      <IonCardHeader>
+        {isNew && "NEW!"}
+        <IonCardSubtitle style={{ textAlign: "left" }}>
+          <div style={{ display: "flex" }}>
+            <span style={{ flex: 1 }}>{contract.projectTitle}</span>
+            {date}
+          </div>
+        </IonCardSubtitle>
+        <IonCardTitle style={{ textAlign: "left" }}>
+          {contract.clause}
+        </IonCardTitle>
+        <IonCardSubtitle style={{ textAlign: "left" }}>
+          <br />
+          {contract.amount && (
+            <div style={{ display: "flex" }}>
+              <span style={{ flex: 1 }}>Privileges</span>
+              {contract.amount / 2}€ / {contract.amount}€
+            </div>
+          )}
+        </IonCardSubtitle>
+        <IonCardSubtitle style={{ textAlign: "left" }}>
+          <br />
+          {contract.dealerFirstName && (
+            <div style={{ display: "flex" }}>
+              <span style={{ flex: 1 }}>{contract.dealerFirstName}</span>
+              {(contract.amount || 0) / 2}€ at{" "}
+              {(contract.interestRate || 0) * 100}%
+            </div>
+          )}
+        </IonCardSubtitle>
+      </IonCardHeader>
+      <IonCardContent></IonCardContent>
+      {isNew && (
+        <IonItem>
+          <IonButton fill="outline" slot="end">
+            Decline
+          </IonButton>
+          <IonButton fill="outline" slot="end">
+            Approve
+          </IonButton>
+          <IonButton fill="outline" slot="end">
+            Chat
+          </IonButton>
+        </IonItem>
+      )}
+    </IonCard>
+  );
+}
--- src/pages/DealTabPage.tsx
+++ src/pages/DealTabPage.tsx
@@ -6,25 +6,19 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
 import "./DealTabPage.css";
-import ProfitLossCard from "../components/ProfitLossCard/ProfitLossCard";
-import {
-  ContractApi,
-  getApiUrl,
-  UserBorrowLendSituationDto,
-} from "../api-clients/api";
+import { ContractApi, getApiUrl, ContractDto } from "../api-clients/api";
+import ContractCard from "../components/ContractCard/ContractCard";
 
 const DealTabPage: React.FC = () => {
-  const [contractApi, setContractApi] = useState<ContractApi>(
-    new ContractApi(getApiUrl())
-  );
-  const [profitLoss, setProfitLoss] = useState<UserBorrowLendSituationDto>();
+  const [contractApi] = useState<ContractApi>(new ContractApi(getApiUrl()));
+  const [contracts, setContracts] = useState<ContractDto[]>([]);
 
   useEffect(() => {
-    contractApi.userSituation()
-    .then(res => setProfitLoss(res))
-    .catch(err => console.log(err));
+    contractApi
+      .listByUserIdAll()
+      .then((res) => setContracts(res))
+      .catch((err) => console.log(err));
   }, []);
 
   return (
@@ -35,7 +29,9 @@ const DealTabPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        {profitLoss && <ProfitLossCard profitLoss={profitLoss} />}
+        {contracts.map((contract, index) => (
+          <ContractCard key={index} contract={contract} isNew={index === 2} />
+        ))}
       </IonContent>
     </IonPage>
   );
